cfbf9759806dd6b4b094267bcb33a0ee8613e391
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2020-07-01 15:54:56 +0200

    fix bugs: json display

--- script.py
+++ script.py
@@ -1,27 +1,28 @@
-from bs4 import BeuatifulSoup
-import requests
-import pymongo
-from flask import Flask
+import pymongo 
+from flask import Flask, jsonify
+import json
 
 URI = 'mongodb://heroku_672rlh8s:s18fo4q6ejpm67lmodcn4hqjil@ds161551.mlab.com:61551/heroku_672rlh8s'
 
 client = pymongo.MongoClient(URI)
 
+app = Flask(__name__)
+
 @app.route('/')
 def connect_db():
-	db = client['heroku_672r1h8s']
+	db = client['heroku_672rlh8s']
 
 	query = {}
 
 	collection = db['covid_stats']
 
-	cursor = collection.find(query)
-
+	cursor = collection.find(query, projection={"_id":0})
 	results = []
 	for doc in cursor:
 		results.append(doc)
+	print(results)
 	return jsonify(results)
 
 if __name__=="__main__":
-	app.run()
+	app.run(debug=True)
 
